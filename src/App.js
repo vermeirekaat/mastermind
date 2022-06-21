01df0c025e6a59ffc934b43dc4cc81f70ee66c12
vermeirekaat <kaat.vermeire@telenet.be>
2022-06-21 09:32:19 +0200

    feat: project setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+
+import './styles/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className={styles.App}>
+      <header className={styles.App__header}>
+        <img src={logo} className={styles.App__logo} alt="logo" />
+        <p className='typo-xl-bold'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.App__link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
